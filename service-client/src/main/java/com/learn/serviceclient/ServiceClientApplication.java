7952d39a8e6ae5d4d136c11bf2d81abaa4090dec
Han1er <guoqing.yuan@hand-china.com>
2020-08-24 16:32:33 +0800

    [ADD]新增服务

--- service-client/src/main/java/com/learn/serviceclient/ServiceClientApplication.java
+++ service-client/src/main/java/com/learn/serviceclient/ServiceClientApplication.java
@@ -1,21 +1,21 @@
-package com.learn.servereureka;
+package com.learn.serviceclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * eureka服务
+ * 注册服务
  *
  * @author Han1er
  * @date 2020-08-24 00:00:00
  */
 @SpringBootApplication
-@EnableEurekaServer
-public class ServerEurekaApplication {
+@EnableEurekaClient
+public class ServiceClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerEurekaApplication.class, args);
+        SpringApplication.run(ServiceClientApplication.class, args);
     }
 
 }
